778102d18d9de7ef81937a29be61c186fa564895
zwachocka <zwachocka@andea.com>
2019-10-23 15:03:28 +0200

    Functions

--- Crafting A Library.py
+++ Crafting A Library.py
@@ -8,25 +8,25 @@ Dodatkowo spróbuj
 2) wyświetl listę książek z nazwą oraz listą autorów w nawiasie."""
 
 class Library():
-    def get_books(self): # init/self? pod rozwagę
+    def get_books(self):
+        print(" #obiekt z klasy Library, ale jak go tutaj zdefiniować")
 
 class Author():
     def __init__(self, author_name):
         self.author_name = author_name
 
 class Book():
+    def __init__(self, book_name, isbn):
+        self.book_name = book_name
+        self.isbn = isbn
+
     def get_author(self, author_name): # jw.
-
-
-
-local_library = Library()
+# gdzieś tu musi być autor, ale jak zrobić odwołanie do klasy Author()?
 
 
 # 2) wyświetlanie listy książek - nim user zechce dodać ksiażkę, musi wiedzieć co już jest
 
-print("Press D to display the list of available books.")
-# .get(book_name) wywołane na słowniku...?
-# co z autorem?
+print("Display the list of available books.")
 
 
 # 1) pętla while, w której będzie się dodawać książkę
@@ -34,9 +34,9 @@ print("Press D to display the list of available books.")
 answer = input("Would you like to donate a book? Y/N")
 while answer == "Y":
     # inputy od usera: book name, author, isbn
-    # metoda dołączania do słownika/tworzenia nowego słownika? --> zdefiniować funkcję typu "createNewBookRecord" i wywołać ją tutaj?
+    #
 
-# pojedyncza książka = słownik/JSON, lista książek --> list? array?"""
+# stworzyć listę o nazwie library z obiektami typu Book
     """"
     book1 = {
         "book_name": "Python for Dummies",
