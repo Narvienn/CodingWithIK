bc4828c3ae041fd2a8c5e4d98a02e707328532f2
zwachocka <zwachocka@andea.com>
2019-10-29 11:07:22 +0100

    Defining add_author

--- Crafting A Library.py
+++ Crafting A Library.py
@@ -1,11 +1,22 @@
-"""Zadanie:
-Stwórz zmienną "library" która będzie przechowywać książki w bibliotece. Książka
-może mieć nazwę, isbn oraz autora, autora (osobna klasa) ustawiane w _init_
-(Doczytasz ;-))
+"""Create a Library class for a library that will store books. A book can have a name, ISBN number and the author
+- author should be defined in a separate class.
 
-Dodatkowo spróbuj
-1) napisać while'a w którym będzie można dodać kolejną książkę.
-2) wyświetl listę książek z nazwą oraz listą autorów w nawiasie."""
+Extras:
+1) do a while loop that will add a book to the library
+2) display a list of books with their names and authors (in brackets)
+
+EXTRA:
+1. napisz metodę która usunie z biblioteki książkę o podanym indeksie
+2. napisz metode która usunie z biblioteki książkę o podanej nazwie
+3. napis metodę która zwróci książki z biblioteki o isbn większym niż nr podany w argumencie
+
+zadanie ekstra:
+spróbuj stworzyć formularz dodawania książek np. def get_book_from_user() który
+- pobierze informacje potrzebne do stworzenia obiektu Book
+- stworzy obiekt klasy Book z podanymi informacjami
+- zwróci obiekt book
+
+"""
 
 
 class Library():
@@ -24,24 +35,30 @@ class Library():
     def get_books(self):
         return self.books
 
+
 class Author():
+    author = ""
+
     def __init__(self, name):
         self.name = name
 
+
 class Book():
+    book = []
+
     def __init__(self, name, isbn):
         self.name = name
         self.isbn = isbn
 
-    def add_author(self, name): # jw.
+    def add_author(self, author):
+        self.book.append(author)
 
 
-
-# stworzyć listę z obiektami typu Book
-item1 = Book("Python for Dummies", "ISBN1234") # + autor
+item1 = Book("Python for Dummies", "ISBN1234")
 item2 = Book("Shakespeare's Collected Works", "ISBN2345")
 
-local_library = Library()
+local_library = Library("MSC Library")
+local_library.books = [item1, item2]
 
 
 """"
@@ -53,6 +70,7 @@ local_library = Library()
 
 answer = input("Would you like to donate a book to the library? Y/N")
 while answer == "Y":
-    # inputy od usera vs wywołanie funkcji add_books, ale na czym i jak przekazać toto do book_roster -.-"""
+    # inputy od usera + wywołanie funkcji add_books, ale na czym i jak przekazać toto do books"""
+
 
 
