172b04141a43327db589025b4745396248641556
zwachocka <zwachocka@andea.com>
2019-12-02 16:38:49 +0100

    PEP-8 alignment & get_books_from_user v.1

--- Crafting a Library 2.0.py
+++ Crafting a Library 2.0.py
@@ -16,57 +16,64 @@ spróbuj stworzyć formularz dodawania książek np. def get_book_from_user() kt
 
 
 class Library():
-   books = None
+    books = None
 
-   def __init__(self, name):
-       self.books = []
-       self.name = name
+    def __init__(self, name):
+        self.books = []
+        self.name = name
 
-   def print_books(self, books):
-       print(books)
+    def print_books(self, books):
+        print(books)
 
-   def add_books(self, book):
-       self.books.append(book)
+    def add_books(self, book):
+        self.books.append(book)
 
-   def get_books(self):
-       return self.books
+    def get_books(self):
+        return self.books
 
 # write a method that will remove a book from the library by its name
-   def remove_book_by_name(self, name):
-       for book in self.books:
-           if book.name == name:
-               idx = self.books.index(book)
-               self.remove_book_by_name(idx) #TODO: HOW TO run this method on an object in console to see if it works?
+    def remove_book_by_name(self, name):
+        for book in self.books:
+            if book.name == name:
+                idx = self.books.index(book)
+                self.remove_book_by_name(idx)  # TODO: HOW TO run this method on an object in console?
 
 # write a method that will remove a book from the library by its index
-   def remove_book_by_index(self, index):
-       del self.books[index]
+    def remove_book_by_index(self, index):
+        del self.books[index]
 
 # write a method that will return any books with ISBN number greater than provided in the argument
-   def get_book_by_isbn_above_number(self):
-       for book in self.books:
-           isbn_number = int(book.isbn.strip("ISBN"))
-           if isbn_number > 1234:
-               return book
+    def get_book_by_isbn_above_number(self):
+        for book in self.books:
+            isbn_number = int(book.isbn.strip("ISBN"))
+            if isbn_number > 1234:
+                return book
+
 
 class Author():
-   author = ""
+    author = ""
 
-   def __init__(self, name):
-       self.name = name
+    def __init__(self, name):
+        self.name = name
 
 
 class Book():
-   book = []
+    book = []
 
-   def __init__(self, name, isbn):
-       self.name = name
-       self.isbn = isbn
+    def __init__(self, name, isbn):
+        self.name = name
+        self.isbn = isbn
 
-   def add_author(self, author):
-       self.book.append(author)
-       return author
+    def add_author(self, author):
+        self.book.append(author)
+        return author
 
-
-   #def get_books_from_user:
-
+    def get_books_from_user(self, book):
+        self.name = input(print("What's the book's name?"))
+        print(self.name)
+        self.book.append(self.name)
+        self.isbn = input("OK. What's the ISBN number of the book?")
+        self.book.append(self.isbn)
+        author = input("And who is the author?")
+        self.book.append(author)
+        return book
